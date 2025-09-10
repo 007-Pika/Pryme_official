a4e84a6aa62da37d2c25c329e3e7c867d5a50316
Pika-Pika <info.qasimtq@gmail.com>
2025-09-10 18:17:37 +1000

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,10 +2,10 @@ import axios from "axios";
 import { getToken } from "./authHelper";
 
 const axiosInstance = axios.create({
-  baseURL: "https://pryme-backend-2khs.onrender.com/api", // add /api
-  withCredentials: true, // only needed if using cookies
+  baseURL: "https://pryme-backend-2khs.onrender.com/api",
 });
 
+// Attach JWT to all requests automatically
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = getToken();
